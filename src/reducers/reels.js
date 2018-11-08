8d8101d915450cbee3cbcafc8fac158953479743
Kevez <kbrady08@gmail.com>
2018-11-08 13:46:59 +0000

    Formatting

--- src/reducers/reels.js
+++ src/reducers/reels.js
@@ -1,15 +1,15 @@
 const initialState = [
 	{
 		id: 0,
-			symbols: ['A', 'B', 'C']
+		symbols: ['A', 'B', 'C']
 	},
 	{
 		id: 1,
-			symbols: ['A', 'B', 'C']
+		symbols: ['A', 'B', 'C']
 	},
 	{
 		id: 2,
-			symbols: ['A', 'B', 'C']
+		symbols: ['A', 'B', 'C']
 	}
 ];
 
@@ -17,7 +17,7 @@ const reels = (state = initialState, action) => {
 	switch (action.type) {
 		case 'UPDATE_REELS':
 			return state.map((reel, idx) => {
-				return { ...reel, symbols: action.newReels[idx] };
+				return {...reel, symbols: action.newReels[idx]};
 			});
 		default:
 			return state;
